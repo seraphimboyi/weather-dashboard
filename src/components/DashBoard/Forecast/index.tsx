8d8dc567241ad597a23151bf551a1f313e628614
boyi <seraphimboyi@users.noreply.github.com>
2025-03-06 17:31:03 +0800

    update: adjust `Forecast` structure and add temperature unit toggle
    - 調整 `Forecast` 元件的結構與樣式
    - 優化 UI 排版，提高使用者體驗

--- src/components/DashBoard/Forecast/index.tsx
+++ src/components/DashBoard/Forecast/index.tsx
@@ -1,28 +1,43 @@
 import React from "react";
 import { mapWeatherCode } from "../data";
 import { WeatherApiResponse } from "../../../types/weatherApi";
+import { Title, Container, Cards, Card, P, CardTitle, Info } from "./styled";
 
 type TypeForecastProps = {
   weatherData: WeatherApiResponse;
+  isCelsius: boolean;
 };
 
-const Forecast: React.FC<TypeForecastProps> = ({ weatherData }) => {
+const Forecast: React.FC<TypeForecastProps> = ({ weatherData, isCelsius }) => {
   const {
     daily: { time, weathercode, temperature_2m_min, temperature_2m_max },
   } = weatherData;
 
+  const convertTemp = (temp: number) =>
+    isCelsius ? temp : (temp * 9) / 5 + 32;
+
   return (
-    <div>
-      <h2>未來 5 天天氣預報</h2>
-      <ul>
+    <Container>
+      <Title>未來 5 天天氣預報</Title>
+      <Cards>
         {time.map((date, index) => (
-          <li key={date}>
-            {date} - {mapWeatherCode(weathercode[index])}
-            {temperature_2m_min[index]}°C ~ {temperature_2m_max[index]}°C
-          </li>
+          <Card key={date}>
+            <CardTitle>日期: {date} </CardTitle>
+            <Info>
+              <P>天氣狀況: {mapWeatherCode(weathercode[index])}</P>
+              <P>
+                最高溫度: {convertTemp(temperature_2m_max[index]).toFixed(1)}°
+                {isCelsius ? "C" : "F"}
+              </P>
+              <P>
+                最低溫度: {convertTemp(temperature_2m_min[index]).toFixed(1)}°
+                {isCelsius ? "C" : "F"}
+              </P>
+            </Info>
+          </Card>
         ))}
-      </ul>
-    </div>
+      </Cards>
+    </Container>
   );
 };
 
