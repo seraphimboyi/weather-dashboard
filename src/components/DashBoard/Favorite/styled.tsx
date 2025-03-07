9f35f5bf0de3413c165a7e6ab195084fdf44e0e8
boyi <seraphimboyi@users.noreply.github.com>
2025-03-07 23:41:33 +0800

    feat: add `Favorite` component to display saved cities
    - 新增 `Favorite` 元件
    - 新增 `styled.tsx` 來管理 `Favorite` 元件的樣式
    - 渲染使用者的最愛城市列表

---
+++ src/components/DashBoard/Favorite/styled.tsx
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+
+type ContainerProps = {
+  $isExpanded: boolean;
+};
+
+type ListProps = {
+  $isExpanded: boolean;
+};
+
+export const Container = styled.div<ContainerProps>`
+  padding-top: 60px;
+  position: fixed;
+  z-index: 1000;
+  left: ${({ $isExpanded }) => ($isExpanded ? "0" : "-250px")};
+  top: 0;
+  width: 250px;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.8);
+  color: white;
+  transition: left 0.3s ease-in-out;
+  box-shadow: ${({ $isExpanded }) =>
+    $isExpanded ? "2px 0px 5px rgba(0,0,0,0.3)" : "none"};
+`;
+
+export const Title = styled.h3`
+  font-size: 24px;
+`;
+
+export const List = styled.div<ListProps>`
+  opacity: ${({ $isExpanded }) => ($isExpanded ? 1 : 0)};
+  transition: opacity 0.3s ease-in-out;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  row-gap: 10px;
+`;
+
+export const Ul = styled.ul``;
+
+export const Li = styled.li`
+  padding: 4px 0px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const CityButton = styled.button`
+  background-color: #c2eb0c;
+  color: black;
+  border: none;
+  font-size: 14px;
+  padding: 4px 10px;
+  cursor: pointer;
+  border-radius: 5px;
+  transition: left 0.3s ease-in-out;
+
+  &:hover {
+    background-color: #5cbf6a;
+  }
+`;
+
+export const RemoveButton = styled.button`
+  background: red;
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  cursor: pointer;
+  border-radius: 4px;
+
+  &:hover {
+    background: darkred;
+  }
+`;
+
+export const ToggleButton = styled.button`
+  position: fixed;
+  left: 12px;
+  top: 10px;
+  background: #007bff;
+  color: white;
+  border: none;
+  padding: 10px 12px;
+  cursor: pointer;
+  border-radius: 50%;
+  transition: left 0.3s ease-in-out;
+
+  &:hover {
+    background: #0056b3;
+  }
+`;
